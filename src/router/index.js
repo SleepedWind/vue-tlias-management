290c6599475da1ced9f6c1cb10c57f371574dfc3
sleepedwind <sleepedwind@163.com>
2025-03-10 22:28:41 +0800

    使用vue-router完成菜单项页面跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LayoutView from '@/views/layout/index.vue'
+import IndexView from '@/views/index/index.vue'
+import LoginView from '@/views/login/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/',
+      name: '' ,
+      component: LayoutView,
+      redirect: '/index',
+      children : [
+        {path: 'index',name: 'index',component: IndexView}
+      ]
+    },
+    {path: '/login',name: 'login',component: LoginView}
   ]
 })
 
