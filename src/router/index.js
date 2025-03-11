56670741f0938e366702b27383cdb5cf58a49fae
sleepedwind <sleepedwind@163.com>
2025-03-11 16:34:32 +0800

    部门管理-列表查询功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LayoutView from '@/views/layout/index.vue'
 import IndexView from '@/views/index/index.vue'
 import LoginView from '@/views/login/index.vue'
+import DeptView from '@/views/dept/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,8 @@ const router = createRouter({
       component: LayoutView,
       redirect: '/index',
       children : [
-        {path: 'index',name: 'index',component: IndexView}
+        {path: 'index',name: 'index',component: IndexView},
+        {path: 'dept',name: 'dept',component: DeptView},
       ]
     },
     {path: '/login',name: 'login',component: LoginView}
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,11 +1,54 @@
 <script setup>
+import {ref,onMounted} from 'vue';
+import axios from 'axios';
+
+//查询
+const search = async() => {
+    let result = await axios.get('https://apifoxmock.com/m1/6003419-5691983-default/depts');
+    if(result.data.code == 1){ //js隐式类型转换，其他数字--true
+      deptList.value = result.data.data;
+    }
+}
+
+const deptList = ref([]);
+
+onMounted(() => {
+  search();
+});
 
 </script>
 
 <template>
-  部门管理
+  <div class="head">
+    <h1>部门管理</h1>
+  </div>
+  
+  <div class="addBtn">
+    <el-button type="primary">+新增部门</el-button>
+  </div>
+
+  <div class="table">
+    <el-table :data="deptList" border style="width: 100%">
+      <el-table-column type="index" label="序号" width="100" align="center"/>
+      <el-table-column prop="name" label="部门名称" width="350" align="center"/>
+      <el-table-column prop="createTime" label="创建日期" width="350" align="center"/>
+      <el-table-column prop="updateTime" label="更新日期" width="350" align="center"/>
+      <el-table-column prop="address" label="操作"  align="center">
+        <el-button type="warning" size="small"><el-icon><Edit /></el-icon>&nbsp&nbsp编辑</el-button>
+        <el-button type="danger" size="small"><el-icon><Delete /></el-icon>&nbsp&nbsp删除</el-button>
+      </el-table-column>
+    </el-table>
+  </div>
+
 </template>
 
 <style scoped>
+.head {
+  margin: 18px 0;
+}
+
+.addBtn {
+  margin: 18px 0;
+}
 
 </style>
