d5bf20cb052ddc08fe6335b66491e54c1333a9da
sleepedwind <sleepedwind@163.com>
2025-03-12 13:46:54 +0800

    部门管理-新增部门功能完成

--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,32 +1,99 @@
 <script setup>
 import {ref,onMounted} from 'vue';
-import axios from 'axios';
+import { queryAllApi,addApi } from '@/api/dept';
+import { ElMessage } from 'element-plus';
 
 //查询
 const search = async() => {
-    let result = await axios.get('https://apifoxmock.com/m1/6003419-5691983-default/depts');
-    if(result.data.code == 1){ //js隐式类型转换，其他数字--true
-      deptList.value = result.data.data;
+    // let result = await axios.get('https://apifoxmock.com/m1/6003419-5691983-default/depts');
+    // if(result.data.code == 1){ //js隐式类型转换，其他数字--true
+    //   deptList.value = result.data.data;
+    // }  
+    let result = await queryAllApi();
+    if(result.code == 1){ //js隐式类型转换，其他数字--true
+      deptList.value = result.data;
     }
 }
 
+//保存部门
+const save = async () => {
+  console.log(deptFormRef.value)
+
+  //表单校验
+  if(!deptFormRef.value) return;
+  deptFormRef.value.validate(async (valid) => { //valid 表示是否校验通过，如果为true则通过 false不通过
+
+      if(valid) {
+        const result = await addApi(dept.value);
+        if(result.code){//成功
+          //提示信息
+          ElMessage.success('操作成功')
+          //关闭对话框
+          dialogFormVisible.value=false;
+          //重新查询
+          search();
+        }else{
+          ElMessage.error(result.msg);
+        }
+      }else {
+        ElMessage.error('表单校验不通过');
+      }
+  })
+
+  
+}
+
+//新增部门
+const addDept = () => {
+  formTitle.value = '新增部门';
+  dialogFormVisible.value = true;
+  dept.value={name:''};
+
+  if(deptFormRef.value) {
+    deptFormRef.value.resetFields();
+  };
+
+}
+
 const deptList = ref([]);
+
+const formTitle = ref('');
+
+const dept = ref({name: ''})
+
+const deptFormRef = ref();
+
+
 
 onMounted(() => {
   search();
 });
 
+//Dilog对话框
+let dialogFormVisible = ref(false);
+
+
+const rules = ref({
+  name: [
+    { required: true, message: '部门名称是必填项', trigger: 'blur' },
+    { min: 2, max: 10, message: '部门名称的长度应该在2~10位之间', trigger: 'blur' },
+  ]
+})
+
 </script>
 
+<!-- 页面标题 -->
 <template>
   <div class="head">
     <h1>部门管理</h1>
   </div>
-  
+
+  <!-- 新增部门按钮 -->
   <div class="addBtn">
-    <el-button type="primary">+新增部门</el-button>
+    <el-button type="primary" @click="addDept">+新增部门</el-button>
   </div>
 
+  <!-- 部门数据表格 -->
   <div class="table">
     <el-table :data="deptList" border style="width: 100%">
       <el-table-column type="index" label="序号" width="100" align="center"/>
@@ -39,6 +106,23 @@ onMounted(() => {
       </el-table-column>
     </el-table>
   </div>
+
+  <!-- 新增部门Dialog对话框 -->
+  <el-dialog v-model="dialogFormVisible" :title=formTitle width="500">
+    <el-form :model="dept" :rules="rules" ref="deptFormRef">
+      <el-form-item label="部门名称" label-width="80px" prop="name">
+        <el-input v-model="dept.name" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="save">
+          确定
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 
 </template>
 
