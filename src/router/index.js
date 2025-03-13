b34d2e24c54af8ba9f2e99093662bedf4a426f23
sleepedwind <sleepedwind@163.com>
2025-03-13 21:48:53 +0800

    员工管理-增删改查功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import LayoutView from '@/views/layout/index.vue'
 import IndexView from '@/views/index/index.vue'
 import LoginView from '@/views/login/index.vue'
 import DeptView from '@/views/dept/index.vue'
+import EmpView from '@/views/emp/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
       children : [
         {path: 'index',name: 'index',component: IndexView},
         {path: 'dept',name: 'dept',component: DeptView},
+        {path: 'emp',name: 'emp',component: EmpView}
       ]
     },
     {path: '/login',name: 'login',component: LoginView}
